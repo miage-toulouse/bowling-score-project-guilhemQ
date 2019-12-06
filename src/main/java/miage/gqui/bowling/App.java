36fdee59cda25bf7386857e85a2871eb2fc65d01
guilhemQ <guilhem.quintana@laposte.net>
2019-12-06 16:07:21 +0100

    first commit

---
+++ src/main/java/miage/gqui/bowling/App.java
@@ -0,0 +1,13 @@
+package miage.gqui.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
